ba1642f85939e13df9b31563ae84aa97ef449bb6
Karan Rajalingam <karanrajalingam@Karans-MacBook-Pro.local>
2019-10-06 13:46:33 -0700

    finish initial setup

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
-        
-        
+        FirebaseApp.configure()        
         return true
     }
 
